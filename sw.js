657d17f054de58aeb37621304d865e6f081961cb
Maless-art <franklinmjj1@gmail.com>
2026-08-19 10:41:05 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="gpi-v0.1.9";
+const CACHE="gpi-v0.2.0";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./catalog.json","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
